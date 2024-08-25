8e4383827fd57c975b1951831b91c266b6607176
NethmiTharushika <nethmitharushika778@gmail.com>
2024-08-25 22:21:51 +0530

    second Commit

--- src/design/pattern/abstractFactory/Rectangle.java
+++ src/design/pattern/abstractFactory/Rectangle.java
@@ -5,7 +5,7 @@ public class Rectangle implements Shape{
     public void draw(){
         for(int i=1;i<=4;i++){
             for(int j=1;j<=6;j++){
-                System.out.print("*");
+                System.out.print("* ");
             }
             System.out.println("");
         }
